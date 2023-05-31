e64ddad491b2c9f6a2247cdbdfd4c91dae1ced2e
atreides <dambrozewicz7@gmail.com>
2023-05-31 19:40:08 +0200

    Exe 2 done

---
+++ cmake-build-debug/exercise2/ut/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/paro/CLionProjects/paro2023-template/exercise2/ut
+# Build directory: /home/paro/CLionProjects/paro2023-template/cmake-build-debug/exercise2/ut
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Scrabble "/home/paro/CLionProjects/paro2023-template/cmake-build-debug/exercise2/ut/ScrabbleTests" "--gtest_output=xml:/home/paro/CLionProjects/paro2023-template/cmake-build-debug/junit/exercise2.junit.xml")
+set_tests_properties(Scrabble PROPERTIES  _BACKTRACE_TRIPLES "/home/paro/CLionProjects/paro2023-template/exercise2/ut/CMakeLists.txt;8;add_test;/home/paro/CLionProjects/paro2023-template/exercise2/ut/CMakeLists.txt;0;")
